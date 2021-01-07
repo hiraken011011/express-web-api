f6c4077db66016fd1a7306ab923d4107f4e09a7b
hiraken011011 <aaa1192aaa2960@gmail.com>
2021-01-07 16:54:04 +0900

    feat: ToDoリストをindex.htmlで表示

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-const express = require('express'); // モジュール読み込み
-const app = express(); // アプリ生成
+const express = require('express'); // expressモジュールを読み込み
+const express = require('multer'); // multerモジュール（ブラウザからのデータ解釈）
+const uuidv4 = require('uuid/v4'); // uuidモジュール（ユニークID生成）
 
+const app = express(); // expressアプリ生成
+app.use(multer().none()); // multerでブラウザから送信されたデータを解釈する
 app.use(express.static('web')); // webフォルダの中身を公開する
 
-// 「localhost:3000/api/v1/list」アクセスで、ToDoを返す
-app.get('/api/v1/list', (req, res) => {
-  // クライアントに送るJSONデータ
-  const todoList = [
-    { title: 'JSを勉強', done: true },
-    { title: 'Nodeを勉強', done: false },
-    { title: 'WebAPIを作成', done: false },
-  ];
+// ToDoリストデータ
+const todoList = [];
 
+// 「localhost:3000/api/v1/list」アクセスで、ToDoリストを返す
+app.get('/api/v1/list', (req, res) => {
   // JSONを送信
   res.json(todoList);
 });
+
+// 「localhost:3000/api/v1/add」アクセスで、ToDoリストに項目を追加
+app.post('/api/v1/add', (req, res) => {
+  // クライアントからの送信データを取得
+  const todoData = req.body;
+  const todoTitle = todoData.title;
+  // ユニークIDを生成する
+  const id = uuidv4();
+  // ToDoの項目を作る
+  const todoItem = {
+    id,
+    title: todoTitle,
+    done: false
+  };
+  
+})
 
 // ポート3000でサーバ立てる
 app.listen(3000, () => console.log('Listening on port 3000'));
